f05d2008dbbc48b599096d92ac2d7011d37dfcc0
vlykingjspr <vlykingjspr@gmail.com>
2024-08-02 21:11:23 +0800

    change database name

--- initialize.php
+++ initialize.php
@@ -6,4 +6,4 @@ if (!defined('dev_data')) define('dev_data', $dev_data);
 if (!defined('DB_SERVER')) define('DB_SERVER', "localhost");
 if (!defined('DB_USERNAME')) define('DB_USERNAME', "root");
 if (!defined('DB_PASSWORD')) define('DB_PASSWORD', "");
-if (!defined('DB_NAME')) define('DB_NAME', "hocms_db");
+if (!defined('DB_NAME')) define('DB_NAME', "iccms");
